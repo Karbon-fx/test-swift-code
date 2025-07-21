42f551e7413e4b6e1de3495eafdb578a4d4ace14
Manvendra Singh <singh.manvendra2002@gmail.com>
2025-07-21 10:01:25 +0000

    Change the bottom padding of footer text to 24px

--- src/components/check-swift-code-form.tsx
+++ src/components/check-swift-code-form.tsx
@@ -166,7 +166,7 @@ export function CheckSwiftCodeFormContent() {
             <p className="font-semibold mt-1 text-[#212121]">{branch.country.toUpperCase()}</p>
           </div>
         </div>
-        <CardFooter className="px-0 pt-6 pb-0">
+        <CardFooter className="px-0 pt-6 pb-6">
           <div className="flex flex-col w-full gap-4">
              <Button variant="outline" onClick={handleSearchAgain} className="w-full text-[#5E5E5E]">
                Search Again
