0bb1c33ccca91a5cd6b6201798bd9cdb3683ba63
Manvendra Singh <singh.manvendra2002@gmail.com>
2025-07-21 09:27:43 +0000

    the color of button is still different. change it to #0657D0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,7 @@ export default {
           foreground: 'hsl(var(--popover-foreground))',
         },
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
+          DEFAULT: '#0657D0',
           foreground: 'hsl(var(--primary-foreground))',
         },
         secondary: {
