b861403798070ca8015a537f65faf894a2d162d3
Manvendra Singh <singh.manvendra2002@gmail.com>
2025-07-22 05:07:28 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div rol

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-card p-1 text-muted-foreground border",
+      "inline-flex h-10 items-center justify-center rounded-md bg-card text-muted-foreground border",
       className
     )}
     {...props}
@@ -30,7 +30,7 @@ const TabsTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow-sm",
-      "text-slate-500",
+      "text-slate-500 p-2",
       className
     )}
     {...props}
