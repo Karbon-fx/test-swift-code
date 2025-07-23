77e3bd845dded95e358e87d9c7eaacd6b86c5400
Manvendra Singh <singh.manvendra2002@gmail.com>
2025-07-23 06:30:08 +0000

    it is still overflowing

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -90,7 +90,7 @@ const SelectContent = React.forwardRef<
     >
         <SelectPrimitive.Viewport
           className={cn(
-            "p-1 w-full",
+            "p-1",
             position === "popper" &&
               "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
           )}
