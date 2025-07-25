d3a49f3e1be5deffc9ea41466c470bdf1c4b1c41
Manvendra Singh <singh.manvendra2002@gmail.com>
2025-07-25 05:14:29 +0000

    Padding fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import { Info } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 sm:p-0">
+    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background sm:p-0">
       <div className="w-full max-w-md">
         <Tabs defaultValue="find" className="w-full">
           <Card className="w-full shadow-sm flex flex-col bg-tool-background">
